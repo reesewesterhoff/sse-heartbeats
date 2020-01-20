d8969a7ecd4702d40fe0d6a402254f3f1eca2c88
Reese Westerhoff <reese.westerhoff@gmail.com>
2020-01-20 11:18:34 -0600

    remove host

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const init = async () => {
   const server = Hapi.server({
     // listener: http2Server,
     port: process.env.PORT || 3000,
-    host: 'localhost'
+    // host: 'localhost'
   });
 
   server.route({
